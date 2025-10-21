11e0cb1a0255522b97b0e32b48afe493cc0d85f1
Marianna Lima <mariannalima@id.uff.br>
2025-10-21 15:21:17 -0300

    Add paste and files

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Cardbutton } from '../cardbutton/cardbutton';
 import { CardButtonCancel } from "../card-button-cancel/card-button-cancel";
 import {MatSliderModule} from '@angular/material/slider';
@@ -18,10 +18,19 @@ interface IInfos {
   standalone: true,
   templateUrl: './card.html',
   styleUrls: ['./card.scss'],
-  imports: [Cardbutton, CardButtonCancel, MatSliderModule],
+  imports: [Cardbutton, MatSliderModule],
+  //CardButtonCancel
 })
 
 export class Card {
+
+  @Input('planType') planType: string = '';
+  @Input({required: true, alias:'planPriceAlias'}) planPrice: number = 0;
+
+  buttonClicked(valueEmitted: boolean){
+    console.log('ButtonClicked');
+  }
+
   plano: IPlano = {
     infos: {
       tipo: 'Simples',
--- src/app/cardbutton/cardbutton.ts
+++ src/app/cardbutton/cardbutton.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cardbutton',
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class Cardbutton {
 
+  @Output('buttonClick') buttonClickEmitter = new EventEmitter<boolean>();
+
+  onButtonClick() {
+    console.log('onButtonClick');
+
+    this.buttonClickEmitter.emit(true);
+  }
 }
